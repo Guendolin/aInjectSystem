10dc53600b4320cc16a1de9439e60ebe5e315468
Guendolin <floggingluna@gmail.com>
2019-08-02 14:16:38 +0200

    Added container script.

---
+++ Container.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace aSystem.aInjectionSystem
+{
+    public class Container : IContainer
+    {
+        private Dictionary<Type, object> _container;
+
+        private bool _shouldDestoryOnLoad;
+        public bool ShouldDestoryOnLoad()
+        {
+            return _shouldDestoryOnLoad;
+        }
+
+        public Container(bool shouldDestoryOnLoad)
+        {
+            _container = new Dictionary<Type, object>();
+            _shouldDestoryOnLoad = shouldDestoryOnLoad;
+        }
+
+        public void Bind<T>(T o)
+        {
+            _container.Add(typeof(T), o);
+        }
+
+        public bool Contains<T>()
+        {
+            return _container.ContainsKey(typeof(T));
+        }
+
+        public T Resolve<T>()
+        {
+            Type type = typeof(T);
+            if (_container.ContainsKey(type))
+            {
+                return (T)_container[type];
+            }
+            return default;
+        }
+
+        public object Resolve(Type type)
+        {
+            if (_container.ContainsKey(type))
+            {
+                return _container[type];
+            }
+            return null;
+        }
+
+        public void Unbind<T>()
+        {
+            Type type = typeof(T);
+            if (_container.ContainsKey(type))
+            {
+                _container.Remove(type);
+            }
+        }
+
+        public void Unbind<T>(T o)
+        {
+            Type type = typeof(T);
+            if (_container.ContainsKey(type))
+            {
+                T content = (T)_container[type];
+                if (!content.Equals(null) && content.Equals(o))
+                {
+                    _container.Remove(type);
+                }
+            }
+        }
+
+        public void Destory()
+        {
+            _container = null;
+        }
+    }
+}
+
+
---
+++ IContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aSystem.aInjectionSystem
+{
+    public interface IContainer
+    {
+		bool ShouldDestoryOnLoad();
+		bool Contains<T>();
+		void Bind<T>(T o);
+		void Unbind<T>();
+		void Unbind<T>(T o);
+		T Resolve<T>();
+		object Resolve(Type T);
+		void Destory();
+    }
+}
